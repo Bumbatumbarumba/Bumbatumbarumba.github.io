bb3d348239ce94f2fd6e42557fb6600f725dce5d
Bartosz <bartosz_@live.ca>
2020-01-28 19:02:49 -0500

    Updates

---
+++ precache-manifest.fed085b32f35866db46fb599fc1d73cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ec969b5432fbf20c359d86a51ee3ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0f31bb89b4b52ffefb5",
+    "url": "/static/js/2.06f6c019.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.06f6c019.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cec46ada5c251d2de44",
+    "url": "/static/js/main.d2ad20e1.chunk.js"
+  },
+  {
+    "revision": "62385ac19bbc0c3b7725",
+    "url": "/static/js/runtime-main.793977a9.js"
+  }
+]);
