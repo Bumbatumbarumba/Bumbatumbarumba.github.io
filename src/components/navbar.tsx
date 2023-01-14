ec33a2c9c93d88b618bd30e2bdad0da2de65215f
bartosz <bartosz_@live.ca>
2023-01-14 01:55:51 -0500

    working on styling; contact page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,30 +10,20 @@ export const Navbar = () => {
             flexDirection: "row",
             justifyContent: "space-evenly"
         }}>
-            <Link to="/" className={selectedLink === "home" ? "active" : ""} onClick={() => setSelectedLink("home")}>
-                <span className={"navbar-link"}>
-                    Home
-                </span>
+            <Link to="/" className={"navbar-link " + (selectedLink === "home" ? "active" : "")} onClick={() => setSelectedLink("home")}>
+                Home
             </Link>
-            <Link to="/edu" className={selectedLink === "edu" ? " active" : ""}>
-                <span className={"navbar-link"} onClick={() => setSelectedLink("edu")}>
-                    Education
-                </span>
+            <Link to="/edu" className={"navbar-link " + (selectedLink === "edu" ? " active" : "")} onClick={() => setSelectedLink("edu")}>
+                Education
             </Link>
-            <Link to="/emp" className={selectedLink === "emp" ? " active" : ""}>
-                <span className={"navbar-link"} onClick={() => setSelectedLink("emp")}>
-                    Employment
-                </span>
+            <Link to="/emp" className={"navbar-link " + (selectedLink === "emp" ? " active" : "")} onClick={() => setSelectedLink("emp")}>
+                Employment
             </Link>
-            <Link to="/prj" className={selectedLink === "prj" ? " active" : ""}>
-                <span className={"navbar-link"} onClick={() => setSelectedLink("prj")}>
-                    Projects
-                </span>
+            <Link to="/prj" className={"navbar-link " + (selectedLink === "prj" ? " active" : "")} onClick={() => setSelectedLink("prj")}>
+                Projects
             </Link>
-            <Link to="/ctt" className={selectedLink === "ctt" ? " active" : ""}>
-                <span className={"navbar-link"} onClick={() => setSelectedLink("ctt")}>
-                    Contact
-                </span>
+            <Link to="/ctt" className={"navbar-link " + (selectedLink === "ctt" ? " active" : "")} onClick={() => setSelectedLink("ctt")}>
+                Contact
             </Link>
         </div>
     );
--- src/views/contact.tsx
+++ src/views/contact.tsx
@@ -2,7 +2,12 @@ import React from "react";
 
 export const Contact = () => {
     return (
-        <>
-        </>
+        <div className="content">
+            <ul style={{ listStyleType: "none", textAlign: "center" }}>
+                <li>bartkosa24(at symbol)gmail.com</li>
+                <li><a className="contact-email" href="https://github.com/Bumbatumbarumba" target={"_blank"} rel="noreferrer">GitHub</a></li>
+                <li><a className="contact-email" href="https://www.linkedin.com/in/bartosz-kosakowski/" target={"_blank"} rel="noreferrer">LinkedIn</a></li>
+            </ul>
+        </div>
     );
 };
